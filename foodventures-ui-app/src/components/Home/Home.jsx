1aa3ecbd355a2f7f7c98b9ddc1b564823cbd83f3
DanoJA888 <djaguilargzz01@gmail.com>
2023-07-06 19:47:24 -0700

    created form in home component that updates fields on change

--- foodventures-ui-app/src/components/Home/Home.jsx
+++ foodventures-ui-app/src/components/Home/Home.jsx
@@ -5,39 +5,67 @@ import axios from "axios";
 export default function Home() {
   const [users, setUsers] = useState([]);
   const [showUser, setShowUser] = useState(false);
+  const [currForm, setForm] = useState({
+    username : '',
+    email: '',
+    password: '',
+  })
+
   const fetchUser = async () => {
     const response = await fetch('http://localhost:3001/user');
     const data = await response.json();
     setUsers(data);
   };
-  const clickShow = async () =>{
-    if(!showUser){
+
+  const clickShow = async () => {
+    if (!showUser) {
       fetchUser();
       setShowUser(!showUser);
-    }
-    else{
+    } else {
       setUsers(false);
       setShowUser(!showUser);
     }
   }
-  return (
-    <div className="home">
-      <p>Home</p>
-      
-      <button onClick={() => clickShow()}>Testing connection</button>
-      <div>
-        {showUser &&
-          <div>
-            {users && users.map((user) => (
-              <div className="user" key={user.password} >
-                <p>{user.username}</p>
-                <p>{user.email}</p>
-                <p>{user.password}</p>
-              </div>
-            ))}
-          </div>
-        }
-      </div>
+
+  const handleChange = (event) => {
+    setForm({
+      ...currForm,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  return (<div className="home">
+    <p>Home</p>
+    <div>
+      <button onClick={
+        () => clickShow()
+      }>Testing connection</button>
+      <div> {
+        showUser && <div> {
+          users && users.map((user) => (<div className="user"
+            key={
+              user.password
+            }>
+            <p> {
+              user.username
+            }</p>
+            <p> {
+              user.email
+            }</p>
+            <p> {
+              user.password
+            }</p>
+          </div>))
+        } </div>
+      } </div>
     </div>
-  )
+    <div>
+      <form action="POST">
+        <input type="text" name="username" id="unId" placeholder="Username" onChange={handleChange}/>
+        <input type="text" name="email" id="emailId" placeholder="Email" onChange={handleChange}/>
+        <input type="text" name="password" id="pwId" placeholder="Password" onChange={handleChange}/>
+        <button>Create Account</button>
+      </form>
+    </div>
+  </div>)
 }
