5c1414747f65f3f42ceb2450f2af38cf5961fa7e
DanoJA888 <djaguilargzz01@gmail.com>
2023-07-07 17:12:01 -0700

    created signup form in signup route

--- foodventures-ui-app/src/components/User/Signup/Signup.jsx
+++ foodventures-ui-app/src/components/User/Signup/Signup.jsx
@@ -1,10 +1,61 @@
-import * as React from "react"
-import "./Signup.css"
+import React, { useState, useContext } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { UserContext } from "../../UserContext.js";
 
 export default function Signup() {
+  const [currInfo, setInfo] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+
+  const { updateUser } = useContext(UserContext);
+  const navigate = useNavigate();
+
+  const handleChange = (event) => {
+    setInfo({
+      ...currForm,
+      [event.target.name]: event.target.value,
+    });
+  };
+
   return (
     <div>
-        <p>SignUp</p>
+      <form >
+        <h2>Sign Up</h2>
+        <div>
+          <label htmlFor="username">Username:</label>
+          <input
+            type="text"
+            id="username"
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            id="email"
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <button type="submit">Sign Up</button>
+        <p>
+          Already have an account?
+          <Link to="/login">Log In</Link>
+        </p>
+      </form>
     </div>
-  )
+  );
 }
