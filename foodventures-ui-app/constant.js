42d0d3ebd89f65f4bd450ec535b1cdfe169d675f
DanoJA888 <djaguilargzz01@gmail.com>
2023-07-10 11:31:36 -0700

    created constants file, removed db fetchs and posts from home

---
+++ foodventures-ui-app/constant.js
@@ -0,0 +1,21 @@
+const cuisines = {
+  "American" : "American",
+  "Asian": "Asian",
+  "British" : "British",
+  "Caribbean" : "Caribbean",
+  "Central Europe" : "Central%20Europe",
+  "Chinese": "Chinese",
+  "Eastern Europe" : "Eastern%20Europe",
+  "French": "French", 
+  "Indian": "Indian",
+  "Italian": "Italian",
+  "Japanese": "Japanese",
+  "Kosher" : "Kosher",
+  "Mediterranean": "Mediterranean",
+  "Mexican": "Mexican",
+  "Middle Eastern": "Middle%20Eastern",
+  "Nordic": "Nordic",
+  "South American": "South%20American",
+  "South East Asian": "South%20East%20Asian"
+};
+
--- foodventures-ui-app/src/components/Home/Home.jsx
+++ foodventures-ui-app/src/components/Home/Home.jsx
@@ -2,43 +2,10 @@ import React, { useState, useContext } from "react";
 import { UserContext } from "../UserContext.js";
 import "./Home.css";
 import axios from "axios";
+import * as constants from "../../../constant.js"
 
 export default function Home() {
   const { currUser, updateUser } = useContext(UserContext);
-  const [users, setUsers] = useState([]);
-  const [showUser, setShowUser] = useState(false);
-  const [currForm, setForm] = useState({
-    username: "",
-    email: "",
-    password: "",
-  });
-
-  const fetchUser = async () => {
-    const response = await fetch("http://localhost:3001/user");
-    const data = await response.json();
-    setUsers(data);
-  };
-
-  const clickShow = async () => {
-    if (!showUser) {
-      fetchUser();
-      setShowUser(!showUser);
-    } else {
-      setUsers(false);
-      setShowUser(!showUser);
-    }
-  };
-
-  const handleChange = (event) => {
-    setForm({
-      ...currForm,
-      [event.target.name]: event.target.value,
-    });
-  };
-
-  const uploadNewUser = async () => {
-    axios.post("http://localhost:3001/user", currForm);
-  };
 
   return (
     <div className="home">
@@ -47,49 +14,6 @@ export default function Home() {
         {currUser &&
           <p>Welcome {currUser.username}</p>
         }
-      </div>
-      <div>
-        <button onClick={() => clickShow()}>Testing connection</button>
-        <div>
-          {showUser && (
-            <div>
-              {users &&
-                users.map((user) => (
-                  <div key={user.password}>
-                    <p> {user.username}</p>
-                    <p> {user.email}</p>
-                    <p> {user.password}</p>
-                  </div>
-                ))}
-            </div>
-          )}
-        </div>
-      </div>
-      <div>
-        <form action="POST">
-          <input
-            type="text"
-            name="username"
-            id="unId"
-            placeholder="Username"
-            onChange={handleChange}
-          />
-          <input
-            type="text"
-            name="email"
-            id="emailId"
-            placeholder="Email"
-            onChange={handleChange}
-          />
-          <input
-            type="text"
-            name="password"
-            id="pwId"
-            placeholder="Password"
-            onChange={handleChange}
-          />
-          <button onClick={() => uploadNewUser()}>Create Account</button>
-        </form>
       </div>
     </div>
   );
