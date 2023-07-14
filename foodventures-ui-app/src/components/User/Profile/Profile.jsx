8920b4906152a450335993344570d1e28dd37c9f
DanoJA888 <djaguilargzz01@gmail.com>
2023-07-14 16:29:51 -0700

    Favorites in profile page now direct users to the recipe detail page

--- foodventures-ui-app/src/components/User/Profile/Profile.jsx
+++ foodventures-ui-app/src/components/User/Profile/Profile.jsx
@@ -1,24 +1,25 @@
 import React, { useEffect, useState, useContext } from "react";
 import { UserContext } from "../../UserContext.js";
+import { Link } from 'react-router-dom';
 import "./Profile.css";
 
 export default function Profile() {
   const { currUser } = useContext(UserContext);
   const [currFavs, setFavs] = useState([]);
   
+  const fetchFavorites = async () => {
+    const response = await fetch("http://localhost:3001/get_favorites", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      credentials: "include",
+    });
+    const data = await response.json();
+    setFavs(data);
+  };
     
   useEffect(() => {
-    const fetchFavorites = async () => {
-        const response = await fetch("http://localhost:3001/get_favorites", {
-          method: "GET",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          credentials: "include",
-        });
-        const data = await response.json();
-        setFavs(data);
-      };
     fetchFavorites();
   }, []);
   console.log(currFavs);
@@ -39,6 +40,7 @@ export default function Profile() {
           <p>WEIGHT {currUser.weight} lbs</p>
         </div>
         <div>
+            <h1>Favorites</h1>
           {currFavs.length === 0 && (
             <div>
               <p>No Favorites</p>
@@ -47,9 +49,10 @@ export default function Profile() {
           {currFavs && (
             <div>
               {currFavs.map((fav) => {
+                const recipeId = fav.recipeId;
                 return (
                   <div>
-                    <p>{fav.recipeName}</p>
+                    <p><Link to= {`/searched/${recipeId}`}><h2>{fav.recipeName}</h2></Link></p>
                   </div>
                 );
               })}
