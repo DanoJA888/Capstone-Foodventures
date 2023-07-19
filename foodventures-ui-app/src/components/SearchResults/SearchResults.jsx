90c1cff4acee0244656573c4ac9340523d3a27cc
DanoJA888 <djaguilargzz01@gmail.com>
2023-07-19 14:25:29 -0700

    created function that pulls recommendations based on the users favorite cuisines, displays either loading message or recommendations once they are ready

--- foodventures-ui-app/src/components/SearchResults/SearchResults.jsx
+++ foodventures-ui-app/src/components/SearchResults/SearchResults.jsx
@@ -8,28 +8,23 @@ export default function SearchResults({cuisine, search, updateSearch}) {
     const {request} = useParams();
   const [currRecipes, updateRecipes] = useState([]);
 
-
-  console.log(cuisine);
-  
+  let url = `https://api.edamam.com/api/recipes/v2?type=public&app_id=${API_ID}&app_key=${API_KEY}`;
+    
+  {if (cuisine){
+      url += `&cuisineType=${cuisine}`;
+  }}
+  {if (search){
+      url += `&q=${search}`;
+  }}
+  const apiCall = async () =>{
+      const response = await fetch(url);
+      const data = await response.json();
+      console.log(url);
+      console.log(data.hits[0].recipe.label);
+      updateRecipes(data.hits);
+  };
 
   useEffect(() =>{
-    let url = `https://api.edamam.com/api/recipes/v2?type=public&app_id=${API_ID}&app_key=${API_KEY}`;
-    
-    {if (cuisine){
-        url += `&cuisineType=${cuisine}`;
-    }}
-  
-    {if (search){
-        url += `&q=${search}`;
-    }}
-    const apiCall = async () =>{
-
-        const response = await fetch(url);
-        const data = await response.json();
-        console.log(url);
-        console.log(data.hits[0].recipe.label);
-        updateRecipes(data.hits);
-    };
     apiCall();
   }, [cuisine, search]);
 
--- foodventures-ui-app/src/components/User/Profile/Profile.jsx
+++ foodventures-ui-app/src/components/User/Profile/Profile.jsx
@@ -2,11 +2,16 @@ import React, { useEffect, useState, useContext } from "react";
 import { UserContext } from "../../UserContext.js";
 import { Link } from 'react-router-dom';
 import "./Profile.css";
+import {API_ID, API_KEY} from "../../../../constant.js";
 
 export default function Profile() {
   const { currUser } = useContext(UserContext);
   const [currFavs, setFavs] = useState([]);
   const [cuisines, setCuisines] = useState([]);
+  const [reccomendations, setReccs] = useState([]);
+  const [cuisinesFetched, setCuisinesFetched] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+
   
   const fetchFavorites = async () => {
     const response = await fetch("http://localhost:3001/get_favorites", {
@@ -19,6 +24,7 @@ export default function Profile() {
     const data = await response.json();
     setFavs(data);
   };
+
   const topCuisines = async () => {
     const response = await fetch("http://localhost:3001/user_cuisines", {
       method: "GET",
@@ -28,13 +34,44 @@ export default function Profile() {
       credentials: "include",
     });
     const data = await response.json();
+    console.log(data.topCuisines)
     setCuisines(data.topCuisines);
+    setCuisinesFetched(true)
   };
+
+  const showReccs = async () =>{
+    let url = `https://api.edamam.com/api/recipes/v2?type=public&app_id=${API_ID}&app_key=${API_KEY}`;
+    let possibleReccs = [];
+    // for loop to fetch recipes from differen cuisines and concats them to an array, where i can hold 60 recipes (important for random cuisine pull)
+    for(let i = 0; i < cuisines.length; i++){
+      const response = await fetch(url+`&cuisineType=${cuisines[i]}`);
+      const data = await response.json();
+      possibleReccs = possibleReccs.concat(data.hits);
+    }
+    let reccs = [];
+    //randomly select 8 recipes from my pool of recipes
+    for(let i = 0; i < 8; i++){
+      reccs.push(possibleReccs[Math.floor(Math.random()*possibleReccs.length)]);
+    }
+    console.log(reccs)
+    setReccs(reccs);
+    setIsLoading(false);
+  }
+
   useEffect(() => {
     fetchFavorites();
     topCuisines();
-  }, []);
-  console.log(cuisines);
+    
+  },[]);
+  // since i have an awaiting expression to fetch the users favorite cuisines and the showReccs function is an async function, 
+  // I have to wait for the cuisines to update in the state in order to make the external api calls.
+  useEffect(() => {
+    if (cuisinesFetched && reccomendations.length === 0) {
+      console.log(reccomendations);
+      showReccs();
+    }
+  }, [cuisinesFetched])
+
 
   return (
     <div>
@@ -71,15 +108,21 @@ export default function Profile() {
             </div>
           )}
           <div>
-            {cuisines &&
-              <div>
-                {cuisines.map((cuisine) => {
-                  return(
-                    <p>{cuisine}</p>
-                  )
-                })}
-              </div>
-            }
+          {isLoading ? (
+            <p>Loading recommendations...</p>
+          ) : (
+            <div>
+              <h1>Recipes You Might Like</h1>
+              {reccomendations.map((rec) => {
+                const recipeId = rec._links.self.href.substring(38, 71);
+                return (
+                  <div>
+                    <Link to= {`/searched/${recipeId}`}><h2>{rec.recipe.label}</h2></Link>
+                  </div>
+                );
+              })}
+            </div>
+          )}
           </div>
         </div>
       </div>
