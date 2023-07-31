bd0f1ab0ee43a6c4fa527e1debd07c0e886dccc2
DanoJA888 <djaguilargzz01@gmail.com>
2023-07-30 18:16:02 -0700

    message warning the user that the search result doesnt return anything

--- foodventures-ui-app/src/components/SearchResults/SearchResults.jsx
+++ foodventures-ui-app/src/components/SearchResults/SearchResults.jsx
@@ -31,20 +31,25 @@ export default function SearchResults({cuisineList, cuisine, search, updateSearc
         }
         <div className="container mt-3 mr-1">
           <div className="row">
-            {currRecipes.map((recipe) => {
-              // using substring method to extract recipeId
-              const recipeId = recipe._links.self.href.substring(38, 71);
-              return (
-                <div className="col-md-3">
-                  <div className="border p-4 text-center">
-                    <img src={recipe.recipe.image} alt={recipe.recipe.label} className="img-fluid" />
-                    <Link to={`/searched/${recipeId}`}>
-                      <h2>{recipe.recipe.label}</h2>
-                    </Link>
+            {currRecipes.length == 0 ? (
+              <h5 className="title">No Recipes Found</h5>
+            ):(
+              currRecipes.map((recipe) => {
+                // using substring method to extract recipeId
+                const recipeId = recipe._links.self.href.substring(38, 71);
+                return (
+                  <div className="col-md-3">
+                    <div className="border p-4 text-center">
+                      <img src={recipe.recipe.image} alt={recipe.recipe.label} className="img-fluid" />
+                      <Link to={`/searched/${recipeId}`}>
+                        <h2>{recipe.recipe.label}</h2>
+                      </Link>
+                    </div>
                   </div>
-                </div>
-              );
-            })}
+                );
+              })
+            )}
+            
           </div>
         </div>
     </div>
