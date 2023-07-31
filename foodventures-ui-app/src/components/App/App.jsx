c5fea71947e645bf5eae7861a36cf4abc6caafb4
DanoJA888 <djaguilargzz01@gmail.com>
2023-07-30 17:20:27 -0700

    made cuisineList a global variable to pass to both home and search

--- foodventures-ui-app/src/components/App/App.jsx
+++ foodventures-ui-app/src/components/App/App.jsx
@@ -20,7 +20,7 @@ export default function App() {
     const storedUser = localStorage.getItem('user');
     return storedUser ? JSON.parse(storedUser) : null;
   });
-
+  const [cuisineList, setCuisineList] = useState([]);
   const [selectedCuisine, updateCuisine] = useState(null);
   const [currSearch, updateSearch] = useState(null);
   const updateUser = (newUser) => {
@@ -38,7 +38,7 @@ export default function App() {
         <main>
           <Navbar resetCuisine = {updateCuisine} resetSearch = {updateSearch}/>
           <Routes>
-            <Route path = '/' element={<Home updateCuisine = {updateCuisine}/>}></Route>
+            <Route path = '/' element={<Home cuisineList  = {cuisineList} setCuisineList = {setCuisineList} updateCuisine = {updateCuisine}/>}></Route>
             <Route path = '/search' element={<Search updateSearch  = {updateSearch} updateCuisine = {updateCuisine}/>}></Route>
             <Route path = '/search_results' element={<SearchResults cuisine = {selectedCuisine} search = {currSearch} updateSearch = {updateSearch}/>}></Route>
             <Route path = '/searched/:recipeId' element={<RecipeInfo/>}></Route>
--- foodventures-ui-app/src/components/Home/Home.jsx
+++ foodventures-ui-app/src/components/Home/Home.jsx
@@ -4,12 +4,12 @@ import { Link } from 'react-router-dom';
 import { fetchCuisines } from "../../../constant.js";
 import "./Home.css";
 
-export default function Home({updateCuisine}) {
+export default function Home({cuisineList, setCuisineList, updateCuisine}) {
   const { currUser, updateUser } = useContext(UserContext);
-  const [cuisines, setCuisines] = useState([]);
+  
   const getCuisines = async () => {
     const data = await fetchCuisines();
-    setCuisines(data);
+    setCuisineList(data);
   };
 
   useEffect(() => {
@@ -20,7 +20,7 @@ export default function Home({updateCuisine}) {
   <div >
     <h1 className="title">Discover Dishes From Around the World!</h1>
     <div className="row mt-3 mx-4 justify-content-center">
-      {cuisines.map((cuisine) => (
+      {cuisineList.map((cuisine) => (
         <div className="col-md-4 d-flex justify-content-center">
           <Link to={`search_results`} className="text-decoration-none text-dark link-cuisine">
               <button
